3374c77f2f599e923d5c8299d188e0e611d000d8
fahmij8 <fahmijabbar@upi.edu>
2021-08-27 04:14:41 +0700

    Add entry point

---
+++ src/scripts/app.ts
@@ -0,0 +1,12 @@
+// Import CSS File
+import "@fontsource/open-sans";
+import "../vendor/soft-ui-dashboard/css/nucleo-icons.css";
+import "../vendor/soft-ui-dashboard/css/nucleo-svg.css";
+import "../vendor/fontawesome/css/fontawesome.css";
+import "../vendor/soft-ui-dashboard/css/soft-ui-dashboard.css";
+import "../styles/custom.css";
+
+// Import JS File
+import "@popperjs/core";
+import "../vendor/soft-ui-dashboard/js/core/bootstrap.min";
+import "../vendor/soft-ui-dashboard/js/soft-ui-dashboard";
