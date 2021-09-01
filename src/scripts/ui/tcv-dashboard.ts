8fdd3a2e7429a06e7fb26d200d44cda4c64ae9c5
fahmij8 <fahmijabbar@upi.edu>
2021-09-02 03:47:39 +0700

    Fix empty data on major display

--- src/scripts/ui/tcv-dashboard.ts
+++ src/scripts/ui/tcv-dashboard.ts
@@ -1,3 +1,4 @@
+import { DataSnapshot } from "firebase/database";
 import dashboard from "../../templates/dashboard.html";
 import { tcv_Display } from "../utilities/display/components";
 import { tcv_FirebaseAuth } from "../utilities/firebase/auth";
@@ -8,7 +9,11 @@ export const displayDashboard = (toRemove: string): void => {
         $(".tcv-content").append(dashboard).addClass("invisible");
         $(".dashboard-image").attr("src", tcv_FirebaseAuth.currentUser().photoURL);
         $(".dashboard-name").html(`${tcv_FirebaseAuth.currentUser().displayName}`);
-        tcv_FirebaseDB.getUserData().then((data) => $(".dashboard-major").html(`${data.val().major}`));
+        tcv_FirebaseDB.getUserData().then((data: DataSnapshot) => {
+            if (data.exists()) {
+                $(".dashboard-major").html(`${data.val().major}`);
+            }
+        });
         $(() => {
             console.log("Good!");
         });
