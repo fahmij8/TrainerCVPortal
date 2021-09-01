83e82f255dffce901f7b7c6f27b9f1f81395cfe8
fahmij8 <fahmijabbar@upi.edu>
2021-09-02 03:49:01 +0700

    Add more initial data for user

--- src/scripts/utilities/firebase/rtdb.ts
+++ src/scripts/utilities/firebase/rtdb.ts
@@ -16,18 +16,37 @@ export const tcv_FirebaseDB = {
         const db = getDatabase();
         get(ref(db, `users/${mailEdited}`))
             .then(async (snapshot) => {
+                const setInputFilter = (textbox: Element, inputFilter: (value: string) => boolean): void => {
+                    ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach((event) => {
+                        textbox.addEventListener(event, function (this: (HTMLInputElement | HTMLTextAreaElement) & { oldValue: string; oldSelectionStart: number | null; oldSelectionEnd: number | null }) {
+                            if (inputFilter(this.value)) {
+                                this.oldValue = this.value;
+                            }
+                        });
+                    });
+                };
+
                 if (!snapshot.exists()) {
                     const { value: formValues } = await Swal.fire({
                         title: "Student Data",
                         html: `
                         <p>Please fill the data carefully!</p>
-                        <form class="needs-validation d-block mx-auto text-start was-validated" style="max-width:400px">
+                        <form class="needs-validation d-block mx-auto text-start" style="max-width:400px">
                             <div class="row">
                                 <div class="col-12">
                                     <div class="form-group">
                                         <label for="tcv-nim">NIM</label>
                                         <input type="number" min="1800000" class="form-control" id="tcv-nim" placeholder="1801389" required>
                                         <div class="invalid-feedback">Please type a valid NIM</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <label for="tcv-tel">WhatsApp Number</label>
+                                        <input type="tel" class="form-control" id="tcv-tel" placeholder="628123456789" value="62" minlength="12" required>
+                                        <div class="invalid-feedback">Please type a valid WhatsApp Number</div>
                                     </div>
                                 </div>
                             </div>
@@ -52,12 +71,19 @@ export const tcv_FirebaseDB = {
                         allowOutsideClick: false,
                         showConfirmButton: true,
                         preConfirm: () => {
+                            $(".needs-validation").addClass("was-validated");
                             const nimValue: string | number | string[] = $("#tcv-nim").val();
                             const majorValue: string | number | string[] = $("#tcv-major").val();
-                            if (nimValue !== "" && majorValue !== null) {
-                                return [nimValue, majorValue];
+                            const telValue: string | number | string[] = $("#tcv-tel").val();
+                            if (nimValue !== "" && majorValue !== null && telValue !== "62" && telValue !== "6") {
+                                return [nimValue, majorValue, telValue];
                             }
                             return false;
+                        },
+                        didOpen: (): void => {
+                            setInputFilter(document.getElementById("tcv-tel"), (value) => {
+                                return /^\d+$/.test(value);
+                            });
                         },
                     });
 
@@ -65,6 +91,8 @@ export const tcv_FirebaseDB = {
                         const data = tcv_FirebaseAuth.currentUser();
                         set(ref(db, `users/${mailEdited}`), {
                             name: data.displayName,
+                            email: data.email,
+                            tel: formValues[2],
                             nim: formValues[0],
                             major: formValues[1],
                             module1_score: {
