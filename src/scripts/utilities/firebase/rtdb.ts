7b353bdc3240c94a9f5f7b796604216018b8067f
fahmij8 <fahmijabbar@upi.edu>
2021-09-09 03:02:04 +0700

    Simplify user data function

--- src/scripts/utilities/firebase/rtdb.ts
+++ src/scripts/utilities/firebase/rtdb.ts
@@ -1,7 +1,9 @@
 import { getDatabase, ref, set, get, DataSnapshot } from "firebase/database";
 import { tcv_HandlerError, tcv_HandleSuccess } from "../display/handler";
 import { tcv_FirebaseAuth } from "./auth";
-import Swal from "sweetalert2";
+import Swal, { SweetAlertResult } from "sweetalert2";
+import { tcv_Templates } from "../display/components";
+import { userData } from "../interface";
 
 export const tcv_FirebaseDB = {
     getUserData: async (): Promise<DataSnapshot> => {
@@ -28,45 +30,9 @@ export const tcv_FirebaseDB = {
                 };
 
                 if (!snapshot.exists()) {
-                    const { value: formValues } = await Swal.fire({
+                    const { value: formValues }: SweetAlertResult = await Swal.fire({
                         title: "Student Data",
-                        html: `
-                        <p>Please fill the data carefully!</p>
-                        <form class="needs-validation d-block mx-auto text-start" style="max-width:400px">
-                            <div class="row">
-                                <div class="col-12">
-                                    <div class="form-group">
-                                        <label for="tcv-nim">NIM</label>
-                                        <input type="number" min="1800000" class="form-control" id="tcv-nim" placeholder="1801389" required>
-                                        <div class="invalid-feedback">Please type a valid NIM</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-12">
-                                    <div class="form-group">
-                                        <label for="tcv-tel">WhatsApp Number</label>
-                                        <input type="tel" class="form-control" id="tcv-tel" placeholder="628123456789" value="62" minlength="12" required>
-                                        <div class="invalid-feedback">Please type a valid WhatsApp Number</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-12">
-                                    <div class="form-group">
-                                        <label for="tcv-major">Major</label>
-                                        <select class="form-select" id="tcv-major" aria-label="Major" required>
-                                            <option value="" selected disabled>Please select one of the option</option>
-                                            <option value="Electrical Engineering & Education">Electrical Engineering & Education</option>
-                                            <option value="Electrical Engineering">Electrical Engineering</option>
-                                            <option value="Automation Engineering & Robotics Education">Automation Engineering & Robotics Education</option>
-                                        </select>
-                                        <div class="invalid-feedback">Please choose one of the options above</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-                        `,
+                        html: tcv_Templates.newUsers,
                         focusConfirm: false,
                         allowEscapeKey: false,
                         allowOutsideClick: false,
@@ -90,48 +56,7 @@ export const tcv_FirebaseDB = {
 
                     if (formValues) {
                         const data = tcv_FirebaseAuth.currentUser();
-                        await set(ref(db, `users/${mailEdited}`), {
-                            name: data.displayName,
-                            email: data.email,
-                            tel: formValues[2],
-                            nim: formValues[0],
-                            major: formValues[1],
-                            module1_score: {
-                                step1: 0,
-                                step2: 0,
-                                step3: 0,
-                                step4: 0,
-                                bonus_score: 0,
-                            },
-                            module2_score: {
-                                step1: 0,
-                                step2: 0,
-                                step3: 0,
-                                step4: 0,
-                                bonus_score: 0,
-                            },
-                            module3_score: {
-                                step1: 0,
-                                step2: 0,
-                                step3: 0,
-                                step4: 0,
-                                bonus_score: 0,
-                            },
-                            module4_score: {
-                                step1: 0,
-                                step2: 0,
-                                step3: 0,
-                                step4: 0,
-                                bonus_score: 0,
-                            },
-                            module5_score: {
-                                step1: 0,
-                                step2: 0,
-                                step3: 0,
-                                step4: 0,
-                                bonus_score: 0,
-                            },
-                        })
+                        await set(ref(db, `users/${mailEdited}`), userData(data, formValues))
                             .then(() => {
                                 tcv_HandleSuccess.show_Confirm("Thank you, have a good day!");
                                 resultInit = true;
