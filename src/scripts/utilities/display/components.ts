487797c9daa8bfc2702d60457562ff74226f9012
fahmij8 <fahmijabbar@upi.edu>
2021-09-09 03:02:17 +0700

    Add new html components

--- src/scripts/utilities/display/components.ts
+++ src/scripts/utilities/display/components.ts
@@ -52,4 +52,49 @@ export const tcv_Display = {
     },
 };
 
-export const tcv_Templates = {};
+export const tcv_Templates = {
+    newUsers: `
+    <p>Please fill the data carefully!</p>
+    <form class="needs-validation d-block mx-auto text-start" style="max-width:400px">
+        <div class="row">
+            <div class="col-12">
+                <div class="form-group">
+                    <label for="tcv-nim">NIM</label>
+                    <input type="number" min="1800000" class="form-control" id="tcv-nim" placeholder="1801389" required>
+                    <div class="invalid-feedback">Please type a valid NIM</div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="form-group">
+                    <label for="tcv-tel">WhatsApp Number</label>
+                    <div class="row">
+                        <div class="col-3">
+                            <input type="text" value="62" class="form-control" disabled="">
+                        </div>
+                        <div class="col-9">
+                            <input type="tel" class="form-control" id="tcv-tel" placeholder="8123456789" minlength="10" required="">
+                            <div class="invalid-feedback">Please type a valid WhatsApp Number</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="form-group">
+                    <label for="tcv-major">Major</label>
+                    <select class="form-select" id="tcv-major" aria-label="Major" required>
+                        <option value="" selected disabled>Please select one of the option</option>
+                        <option value="Electrical Engineering & Education">Electrical Engineering & Education</option>
+                        <option value="Electrical Engineering">Electrical Engineering</option>
+                        <option value="Automation Engineering & Robotics Education">Automation Engineering & Robotics Education</option>
+                    </select>
+                    <div class="invalid-feedback">Please choose one of the options above</div>
+                </div>
+            </div>
+        </div>
+    </form>
+    `,
+};
