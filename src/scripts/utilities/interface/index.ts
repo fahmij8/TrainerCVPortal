e15263f7f98375090fc2165a9776a33b9ca7ad3c
fahmij8 <fahmijabbar@upi.edu>
2021-09-09 03:01:38 +0700

    new data types/interfaces

--- src/scripts/utilities/interface/index.ts
+++ src/scripts/utilities/interface/index.ts
@@ -1,3 +1,5 @@
+import { User } from "@firebase/auth";
+
 export interface StringKeyObject {
     [key: string]: string;
 }
@@ -5,3 +7,95 @@ export interface StringKeyObject {
 export interface NumberKeyObject {
     [key: number]: number;
 }
+
+export interface StringKeyNumberValueObject {
+    [key: string]: number;
+}
+
+interface UserData {
+    name: string;
+    email: string;
+    tel: string;
+    nim: string;
+    major: string;
+    module1_score: {
+        step1: number;
+        step2: number;
+        step3: number;
+        step4: number;
+        bonus_score: number;
+    };
+    module2_score: {
+        step1: number;
+        step2: number;
+        step3: number;
+        step4: number;
+        bonus_score: number;
+    };
+    module3_score: {
+        step1: number;
+        step2: number;
+        step3: number;
+        step4: number;
+        bonus_score: number;
+    };
+    module4_score: {
+        step1: number;
+        step2: number;
+        step3: number;
+        step4: number;
+        bonus_score: number;
+    };
+    module5_score: {
+        step1: number;
+        step2: number;
+        step3: number;
+        step4: number;
+        bonus_score: number;
+    };
+}
+
+export const userData = (data: User, formValues: [string, string, string]): UserData => {
+    return {
+        name: data.displayName,
+        email: data.email,
+        tel: `62${formValues[2]}`,
+        nim: formValues[0],
+        major: formValues[1],
+        module1_score: {
+            step1: 0,
+            step2: 0,
+            step3: 0,
+            step4: 0,
+            bonus_score: 0,
+        },
+        module2_score: {
+            step1: 0,
+            step2: 0,
+            step3: 0,
+            step4: 0,
+            bonus_score: 0,
+        },
+        module3_score: {
+            step1: 0,
+            step2: 0,
+            step3: 0,
+            step4: 0,
+            bonus_score: 0,
+        },
+        module4_score: {
+            step1: 0,
+            step2: 0,
+            step3: 0,
+            step4: 0,
+            bonus_score: 0,
+        },
+        module5_score: {
+            step1: 0,
+            step2: 0,
+            step3: 0,
+            step4: 0,
+            bonus_score: 0,
+        },
+    };
+};
