5fbfd2dbdc98e8fc776d0987ba451c85edb9ad44
fahmij8 <fahmijabbar@upi.edu>
2021-09-14 20:41:19 +0700

    Add new realtime database function

--- src/scripts/utilities/firebase/rtdb.ts
+++ src/scripts/utilities/firebase/rtdb.ts
@@ -1,9 +1,11 @@
-import { getDatabase, ref, set, get, DataSnapshot } from "firebase/database";
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { getDatabase, ref, set, get, DataSnapshot, remove } from "firebase/database";
 import { tcv_HandlerError, tcv_HandleSuccess } from "../display/handler";
 import { tcv_FirebaseAuth } from "./auth";
 import Swal, { SweetAlertResult } from "sweetalert2";
 import { tcv_Templates } from "../display/components";
-import { userData } from "../interface";
+import { userDataObjects } from "../interface";
 
 export const tcv_FirebaseDB = {
     getUserData: async (): Promise<DataSnapshot> => {
@@ -11,6 +13,30 @@ export const tcv_FirebaseDB = {
         const mailEdited = user.email.replace(".", "");
         const db = getDatabase();
         return await get(ref(db, `users/${mailEdited}`));
+    },
+    getSBCStatus: async (): Promise<DataSnapshot> => {
+        const db = getDatabase();
+        return await get(ref(db, `sbc/status`));
+    },
+    getScheduleData: async (): Promise<DataSnapshot> => {
+        const db = getDatabase();
+        return await get(ref(db, `schedule`));
+    },
+    getSBCData: async (): Promise<DataSnapshot> => {
+        const db = getDatabase();
+        return await get(ref(db, `sbc`));
+    },
+    getCamData: async (): Promise<DataSnapshot> => {
+        const db = getDatabase();
+        return await get(ref(db, `campassword`));
+    },
+    postData: async (dir: string, data: any): Promise<void> => {
+        const db = getDatabase();
+        return await set(ref(db, dir), data);
+    },
+    removeData: async (dir: string): Promise<void> => {
+        const db = getDatabase();
+        return await remove(ref(db, dir));
     },
     async initUserData(): Promise<boolean> {
         const user = tcv_FirebaseAuth.currentUser();
@@ -56,7 +82,7 @@ export const tcv_FirebaseDB = {
 
                     if (formValues) {
                         const data = tcv_FirebaseAuth.currentUser();
-                        await set(ref(db, `users/${mailEdited}`), userData(data, formValues))
+                        await set(ref(db, `users/${mailEdited}`), userDataObjects(data, formValues))
                             .then(() => {
                                 tcv_HandleSuccess.show_Confirm("Thank you, have a good day!");
                                 resultInit = true;
