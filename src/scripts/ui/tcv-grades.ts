e6c9821448190cfad766836ff9ab087a8815a565
fahmij8 <fahmijabbar@upi.edu>
2021-09-14 20:41:41 +0700

    Add error handling on grade page

--- src/scripts/ui/tcv-grades.ts
+++ src/scripts/ui/tcv-grades.ts
@@ -7,106 +7,113 @@ import { DataSnapshot } from "@firebase/database";
 import { tcv_Util } from "../utilities/display/util";
 import { Chart } from "../../vendor/soft-ui-dashboard/js/plugins/chartjs.min";
 import grades from "../../templates/grades.html";
+import { tcv_HandlerError } from "../utilities/display/handler";
 
 export const displayGrades = (toRemove: string): void => {
     tcv_Display.displayContent(async () => {
         $(".tcv-content").append(grades).addClass("invisible");
         import("../../vendor/soft-ui-dashboard/js/plugins/smooth-scrollbar.min");
         // Fetching Data
-        tcv_FirebaseDB.getUserData().then(async (data: DataSnapshot) => {
-            if (data.exists()) {
-                const userData = data.val();
-                // Count & Charts overall progress
-                let modules_finished = 0;
-                let modules_inprogress = 0;
-                let modules_notworked = 0;
-                const module_scores = [userData.module1_score, userData.module2_score, userData.module3_score, userData.module4_score, userData.module5_score];
-                module_scores.forEach((modules, index) => {
-                    const checkResult = tcv_Util.checkModulesOverall(modules);
-                    if (checkResult[0] === "finished") {
-                        modules_finished += 1;
-                        $(`.tcv-progress-module${index + 1}`).addClass("bg-gradient-success w-100");
-                    } else if (checkResult[0] === "in progress") {
-                        modules_inprogress += 1;
-                        $(`.tcv-progress-module${index + 1}`).addClass(`bg-gradient-warning w-${checkResult[1] * 25}`);
-                    } else {
-                        modules_notworked += 1;
-                        $(`.tcv-progress-module${index + 1}`).addClass("bg-gradient-secondary w-5");
-                    }
-                    // Details progress bar
-                    $(`.tcv-progress-module${index + 1}-text`).html(`${checkResult[1] * 25}%`);
-                    // Render data on tables
-                    $(".tcv-scores").append(tcv_Templates.scoresSumary(modules, checkResult[0], index));
-                });
-                $(".tcv-grades-overall").html(`${modules_finished} Modules`);
+        tcv_FirebaseDB
+            .getUserData()
+            .then(async (data: DataSnapshot) => {
+                if (data.exists()) {
+                    const userData = data.val();
+                    // Count & Charts overall progress
+                    let modules_finished = 0;
+                    let modules_inprogress = 0;
+                    let modules_notworked = 0;
+                    const module_scores = [userData.module1_score, userData.module2_score, userData.module3_score, userData.module4_score, userData.module5_score];
+                    module_scores.forEach((modules, index) => {
+                        const checkResult = tcv_Util.checkModulesOverall(modules);
+                        if (checkResult[0] === "finished") {
+                            modules_finished += 1;
+                            $(`.tcv-progress-module${index + 1}`).addClass("bg-gradient-success w-100");
+                        } else if (checkResult[0] === "in progress") {
+                            modules_inprogress += 1;
+                            $(`.tcv-progress-module${index + 1}`).addClass(`bg-gradient-warning w-${checkResult[1] * 25}`);
+                        } else {
+                            modules_notworked += 1;
+                            $(`.tcv-progress-module${index + 1}`).addClass("bg-gradient-secondary w-5");
+                        }
+                        // Details progress bar
+                        $(`.tcv-progress-module${index + 1}-text`).html(`${checkResult[1] * 25}%`);
+                        // Render data on tables
+                        $(".tcv-scores").append(tcv_Templates.scoresSumary(modules, checkResult[0], index));
+                    });
+                    $(".tcv-grades-overall").html(`${modules_finished} Modules`);
 
-                const chartGradeOveralll: HTMLCanvasElement = <HTMLCanvasElement>$("#tcv-chart-grade-overall").get(0);
-                const ctx1: CanvasRenderingContext2D = chartGradeOveralll.getContext("2d");
+                    const chartGradeOveralll: HTMLCanvasElement = <HTMLCanvasElement>$("#tcv-chart-grade-overall").get(0);
+                    const ctx1: CanvasRenderingContext2D = chartGradeOveralll.getContext("2d");
 
-                new Chart(ctx1, {
-                    type: "doughnut",
-                    data: {
-                        labels: ["Done", "In progress", "Incompleted"],
-                        datasets: [
-                            {
-                                label: "Computer Vision Modules - Overall Progress",
-                                weight: 9,
-                                cutout: 50,
-                                tension: 0.9,
-                                pointRadius: 2,
-                                borderWidth: 2,
-                                backgroundColor: ["#8CE62E", "#F9A035", "#a8b8d8"],
-                                data: [modules_finished, modules_inprogress, modules_notworked],
-                                fill: false,
+                    new Chart(ctx1, {
+                        type: "doughnut",
+                        data: {
+                            labels: ["Done", "In progress", "Incompleted"],
+                            datasets: [
+                                {
+                                    label: "Computer Vision Modules - Overall Progress",
+                                    weight: 9,
+                                    cutout: 50,
+                                    tension: 0.9,
+                                    pointRadius: 2,
+                                    borderWidth: 2,
+                                    backgroundColor: ["#8CE62E", "#F9A035", "#a8b8d8"],
+                                    data: [modules_finished, modules_inprogress, modules_notworked],
+                                    fill: false,
+                                },
+                            ],
+                        },
+                        options: {
+                            responsive: true,
+                            maintainAspectRatio: false,
+                            plugins: {
+                                legend: {
+                                    display: false,
+                                },
                             },
-                        ],
-                    },
-                    options: {
-                        responsive: true,
-                        maintainAspectRatio: false,
-                        plugins: {
-                            legend: {
-                                display: false,
+                            interaction: {
+                                intersect: false,
+                                mode: "index",
+                            },
+                            scales: {
+                                y: {
+                                    grid: {
+                                        drawBorder: false,
+                                        display: false,
+                                        drawOnChartArea: false,
+                                        drawTicks: false,
+                                    },
+                                    ticks: {
+                                        display: false,
+                                    },
+                                },
+                                x: {
+                                    grid: {
+                                        drawBorder: false,
+                                        display: false,
+                                        drawOnChartArea: false,
+                                        drawTicks: false,
+                                    },
+                                    ticks: {
+                                        display: false,
+                                    },
+                                },
                             },
                         },
-                        interaction: {
-                            intersect: false,
-                            mode: "index",
-                        },
-                        scales: {
-                            y: {
-                                grid: {
-                                    drawBorder: false,
-                                    display: false,
-                                    drawOnChartArea: false,
-                                    drawTicks: false,
-                                },
-                                ticks: {
-                                    display: false,
-                                },
-                            },
-                            x: {
-                                grid: {
-                                    drawBorder: false,
-                                    display: false,
-                                    drawOnChartArea: false,
-                                    drawTicks: false,
-                                },
-                                ticks: {
-                                    display: false,
-                                },
-                            },
-                        },
-                    },
-                });
+                    });
 
-                // Initializing datatables
-                new simpleDatatables.DataTable("#datatable-basic", {
-                    searchable: false,
-                    fixedHeight: true,
-                });
-            }
-        });
+                    // Initializing datatables
+                    new simpleDatatables.DataTable("#tcv-grades", {
+                        searchable: false,
+                        fixedHeight: true,
+                    });
+                }
+            })
+            .catch((error) => {
+                console.error(error);
+                tcv_HandlerError.show_NoConfirm("Failed to fetch user data, please check your internet connection");
+            });
 
         $(() => {
             // Tootltip Initialization
