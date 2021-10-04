144736bb743c2be85960e2382663306e8d297004
fahmij8 <fahmijabbar@upi.edu>
2021-10-05 02:07:03 +0700

    Add function in RTDB

--- src/scripts/utilities/firebase/rtdb.ts
+++ src/scripts/utilities/firebase/rtdb.ts
@@ -1,11 +1,9 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { getDatabase, ref, set, get, DataSnapshot, remove } from "firebase/database";
-import { tcv_HandlerError, tcv_HandleSuccess } from "../display/handler";
+import { tcv_HandlerError } from "../display/handler";
 import { tcv_FirebaseAuth } from "./auth";
-import Swal, { SweetAlertResult } from "sweetalert2";
-import { tcv_Templates } from "../display/components";
-import { userDataObjects } from "../interface";
+import { displayInitUser } from "../../ui/tcv-init-user";
 
 export const tcv_FirebaseDB = {
     getUserData: async (): Promise<DataSnapshot> => {
@@ -45,57 +43,12 @@ export const tcv_FirebaseDB = {
         let resultInit = false;
         await get(ref(db, `users/${mailEdited}`))
             .then(async (snapshot) => {
-                const setInputFilter = (textbox: Element, inputFilter: (value: string) => boolean): void => {
-                    ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach((event) => {
-                        textbox.addEventListener(event, function (this: (HTMLInputElement | HTMLTextAreaElement) & { oldValue: string; oldSelectionStart: number | null; oldSelectionEnd: number | null }) {
-                            if (inputFilter(this.value)) {
-                                this.oldValue = this.value;
-                            }
-                        });
+                await displayInitUser(snapshot, mailEdited)
+                    .then((result) => (resultInit = result))
+                    .catch((error) => {
+                        console.error(error);
+                        tcv_HandlerError.show_NoConfirm("Firebase RTDB failed to initialize user data");
                     });
-                };
-
-                if (!snapshot.exists()) {
-                    const { value: formValues }: SweetAlertResult = await Swal.fire({
-                        title: "Student Data",
-                        html: tcv_Templates.newUsers,
-                        focusConfirm: false,
-                        allowEscapeKey: false,
-                        allowOutsideClick: false,
-                        showConfirmButton: true,
-                        preConfirm: () => {
-                            $(".needs-validation").addClass("was-validated");
-                            const nimValue: string | number | string[] = $("#tcv-nim").val();
-                            const majorValue: string | number | string[] = $("#tcv-major").val();
-                            const telValue: string | number | string[] = $("#tcv-tel").val();
-                            if (nimValue !== "" && majorValue !== null && telValue !== "62" && telValue !== "6") {
-                                return [nimValue, majorValue, telValue];
-                            }
-                            return false;
-                        },
-                        didOpen: (): void => {
-                            setInputFilter(document.getElementById("tcv-tel"), (value) => {
-                                return /^\d+$/.test(value);
-                            });
-                        },
-                    });
-
-                    if (formValues) {
-                        const data = tcv_FirebaseAuth.currentUser();
-                        await set(ref(db, `users/${mailEdited}`), userDataObjects(data, formValues))
-                            .then(() => {
-                                tcv_HandleSuccess.show_Confirm("Thank you, have a good day!");
-                                resultInit = true;
-                            })
-                            .catch((error) => {
-                                console.error(error);
-                                tcv_HandlerError.show_NoConfirm("Firebase RTDB failed to submit user data");
-                                resultInit = false;
-                            });
-                    }
-                } else {
-                    resultInit = true;
-                }
             })
             .catch((error) => {
                 console.error(error);
