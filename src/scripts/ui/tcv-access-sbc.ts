7e1e1353578dc083b60d70404f1289a8e912cef4
fahmij8 <fahmijabbar@upi.edu>
2021-10-07 05:51:32 +0700

    On progress, Calendar view

--- src/scripts/ui/tcv-access-sbc.ts
+++ src/scripts/ui/tcv-access-sbc.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import access_sbc from "../../templates/access-sbc.html";
 import { DataSnapshot } from "firebase/database";
 import { tcv_FirebaseAuth } from "../utilities/firebase/auth";
@@ -10,6 +11,7 @@ import Swal, { SweetAlertResult } from "sweetalert2";
 import flatpickr from "flatpickr";
 import confirmDate from "flatpickr/dist/plugins/confirmDate/confirmDate";
 import PerfectScrollbar from "perfect-scrollbar";
+import { FullCalendar } from "../../vendor/soft-ui-dashboard/js/plugins/fullcalendar.min";
 
 export const displayAccessSBC = (toRemove: string): void => {
     tcv_Display.displayContent(async () => {
@@ -309,6 +311,115 @@ export const displayAccessSBC = (toRemove: string): void => {
                         });
                 });
             }
+            const calendar = new (FullCalendar as any).Calendar(document.getElementById("calendar"), {
+                height: "100%",
+                contentHeight: "auto",
+                aspectRatio: 2,
+                initialView: "dayGridMonth",
+                headerToolbar: {
+                    start: "title", // will normally be on the left. if RTL, will be on the right
+                    center: "",
+                    end: "today prev,next", // will normally be on the right. if RTL, will be on the left
+                },
+                selectable: true,
+                editable: true,
+                initialDate: "2020-12-01",
+                events: [
+                    {
+                        title: "Call with Dave",
+                        start: "2020-11-18",
+                        end: "2020-11-18",
+                        className: "bg-gradient-danger",
+                    },
+
+                    {
+                        title: "Lunch meeting",
+                        start: "2020-11-21",
+                        end: "2020-11-22",
+                        className: "bg-gradient-warning",
+                    },
+
+                    {
+                        title: "All day conference",
+                        start: "2020-11-29",
+                        end: "2020-11-29",
+                        className: "bg-gradient-success",
+                    },
+
+                    {
+                        title: "Meeting with Mary",
+                        start: "2020-12-01",
+                        end: "2020-12-01",
+                        className: "bg-gradient-info",
+                    },
+
+                    {
+                        title: "Winter Hackaton",
+                        start: "2020-12-03",
+                        end: "2020-12-03",
+                        className: "bg-gradient-danger",
+                    },
+
+                    {
+                        title: "Digital event",
+                        start: "2020-12-07",
+                        end: "2020-12-09",
+                        className: "bg-gradient-warning",
+                    },
+
+                    {
+                        title: "Marketing event",
+                        start: "2020-12-10",
+                        end: "2020-12-10",
+                        className: "bg-gradient-primary",
+                    },
+
+                    {
+                        title: "Dinner with Family",
+                        start: "2020-12-19",
+                        end: "2020-12-19",
+                        className: "bg-gradient-danger",
+                    },
+
+                    {
+                        title: "Black Friday",
+                        start: "2020-12-23",
+                        end: "2020-12-23",
+                        className: "bg-gradient-info",
+                    },
+
+                    {
+                        title: "Cyber Week",
+                        start: "2020-12-02",
+                        end: "2020-12-02",
+                        className: "bg-gradient-warning",
+                    },
+                ],
+                views: {
+                    month: {
+                        titleFormat: {
+                            month: "long",
+                            year: "numeric",
+                        },
+                    },
+                    agendaWeek: {
+                        titleFormat: {
+                            month: "long",
+                            year: "numeric",
+                            day: "numeric",
+                        },
+                    },
+                    agendaDay: {
+                        titleFormat: {
+                            month: "short",
+                            year: "numeric",
+                            day: "numeric",
+                        },
+                    },
+                },
+            });
+
+            calendar.render();
         });
     }, toRemove);
 };
