a5799408ef7870f7a2d69f0c5e259e26d5cc2e08
fahmij8 <fahmijabbar@upi.edu>
2021-10-09 01:29:54 +0700

    Add fallback for error user image in dashboard

--- src/scripts/ui/tcv-dashboard.ts
+++ src/scripts/ui/tcv-dashboard.ts
@@ -1,5 +1,6 @@
 import { DataSnapshot } from "firebase/database";
 import dashboard from "../../templates/dashboard.html";
+import userPic from "../../assets/user.png";
 import { tcv_Display } from "../utilities/display/components";
 import { tcv_HandleWarning } from "../utilities/display/handler";
 import { tcv_FirebaseAuth } from "../utilities/firebase/auth";
@@ -9,6 +10,9 @@ export const displayDashboard = (toRemove: string): void => {
     tcv_Display.displayContent(() => {
         $(".tcv-content").append(dashboard).addClass("invisible");
         $(".dashboard-image").attr("src", tcv_FirebaseAuth.currentUser().photoURL);
+        $(".dashboard-image").on("error", () => {
+            $(".dashboard-image").attr("src", userPic);
+        });
         $(".dashboard-name").html(`${tcv_FirebaseAuth.currentUser().displayName}`);
         tcv_FirebaseDB.getUserData().then((data: DataSnapshot) => {
             if (data.exists()) {
