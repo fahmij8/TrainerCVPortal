e5b3413b5d6592481c91761a8c1cbba3e25517dd
fahmij8 <fahmijabbar@upi.edu>
2021-10-15 03:10:05 +0700

    Remove perfect scrollbar

--- src/vendor/soft-ui-dashboard/js/soft-ui-dashboard.js
+++ src/vendor/soft-ui-dashboard/js/soft-ui-dashboard.js
@@ -3,33 +3,32 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-undef */
 import * as bootstrap from "./core/bootstrap.min";
-import PerfectScrollbar from "perfect-scrollbar";
-("use strict");
+// import PerfectScrollbar from "perfect-scrollbar";
 
-var isWindows = navigator.platform.indexOf("Win") > -1 ? true : true;
+// var isWindows = navigator.platform.indexOf("Win") > -1 ? true : true;
 
-if (isWindows) {
-    // if we are on windows OS we activate the perfectScrollbar function
-    if (document.getElementsByClassName("main-content")[0]) {
-        var mainpanel = document.querySelector(".main-content");
-        var ps = new PerfectScrollbar(mainpanel);
-    }
+// if (isWindows) {
+//     // if we are on windows OS we activate the perfectScrollbar function
+//     if (document.getElementsByClassName("main-content")[0]) {
+//         var mainpanel = document.querySelector(".main-content");
+//         var ps = new PerfectScrollbar(mainpanel, optionsPS);
+//     }
 
-    if (document.getElementsByClassName("sidenav")[0]) {
-        var sidebar = document.querySelector(".sidenav");
-        var ps1 = new PerfectScrollbar(sidebar);
-    }
+//     if (document.getElementsByClassName("sidenav")[0]) {
+//         var sidebar = document.querySelector(".sidenav");
+//         var ps1 = new PerfectScrollbar(sidebar, optionsPS);
+//     }
 
-    if (document.getElementsByClassName("navbar-collapse")[0]) {
-        var fixedplugin = document.querySelector(".navbar-collapse");
-        var ps2 = new PerfectScrollbar(fixedplugin);
-    }
+//     if (document.getElementsByClassName("navbar-collapse")[0]) {
+//         var fixedplugin = document.querySelector(".navbar-collapse");
+//         var ps2 = new PerfectScrollbar(fixedplugin, optionsPS);
+//     }
 
-    if (document.getElementsByClassName("fixed-plugin")[0]) {
-        var fixedplugin = document.querySelector(".fixed-plugin");
-        var ps3 = new PerfectScrollbar(fixedplugin);
-    }
-}
+//     if (document.getElementsByClassName("fixed-plugin")[0]) {
+//         var fixedplugin = document.querySelector(".fixed-plugin");
+//         var ps3 = new PerfectScrollbar(fixedplugin, optionsPS);
+//     }
+// }
 
 // Verify navbar blur on scroll
 if (document.getElementById("navbarBlur")) {
