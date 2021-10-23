90b968047972c84b1e0c3bfbe01919a060b9e771
fahmij8 <fahmijabbar@upi.edu>
2021-10-23 16:34:45 +0700

    Unuse realtime database On progress

--- src/scripts/utilities/firebase/rtdb.ts
+++ src/scripts/utilities/firebase/rtdb.ts
@@ -1,12 +1,12 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { getDatabase, ref, set, get, DataSnapshot, remove } from "firebase/database";
+import { getDatabase, ref, set, get, DataSnapshot, remove, onDisconnect, onValue } from "firebase/database";
 import { tcv_HandlerError } from "../display/handler";
 import { tcv_FirebaseAuth } from "./auth";
 import { displayInitUser } from "../../ui/tcv-init-user";
 
 export const tcv_FirebaseDB = {
-    getUserData: async (): Promise<DataSnapshot> => {
+    async getUserData(): Promise<DataSnapshot> {
         const user = tcv_FirebaseAuth.currentUser();
         const mailEdited = user.email.replace(".", "");
         const db = getDatabase();
@@ -36,24 +36,24 @@ export const tcv_FirebaseDB = {
         const db = getDatabase();
         return await remove(ref(db, dir));
     },
-    async initUserData(): Promise<boolean> {
-        const user = tcv_FirebaseAuth.currentUser();
-        const mailEdited = user.email.replace(".", "");
-        const db = getDatabase();
-        let resultInit = false;
-        await get(ref(db, `users/${mailEdited}`))
-            .then(async (snapshot) => {
-                await displayInitUser(snapshot, mailEdited)
-                    .then((result) => (resultInit = result))
-                    .catch((error) => {
-                        console.error(error);
-                        tcv_HandlerError.show_NoConfirm("Firebase RTDB failed to initialize user data");
-                    });
-            })
-            .catch((error) => {
-                console.error(error);
-                tcv_HandlerError.show_NoConfirm("Firebase RTDB failed to check user data");
-            });
-        return resultInit;
-    },
+    // async initUserData(): Promise<boolean> {
+    //     const user = tcv_FirebaseAuth.currentUser();
+    //     const mailEdited = user.email.replace(".", "");
+    //     const db = getDatabase();
+    //     let resultInit = false;
+    //     await get(ref(db, `users/${mailEdited}`))
+    //         .then(async (snapshot) => {
+    //             await displayInitUser(snapshot, mailEdited)
+    //                 .then((result) => (resultInit = result))
+    //                 .catch((error) => {
+    //                     console.error(error);
+    //                     tcv_HandlerError.show_NoConfirm("Firebase RTDB failed to initialize user data");
+    //                 });
+    //         })
+    //         .catch((error) => {
+    //             console.error(error);
+    //             tcv_HandlerError.show_NoConfirm("Firebase RTDB failed to check user data");
+    //         });
+    //     return resultInit;
+    // },
 };
