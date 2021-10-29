f4afdd02b7bd8edbf427c092646d396c753a6d73
fahmij8 <fahmijabbar@upi.edu>
2021-10-29 16:14:43 +0700

    Switching from rtdb to firestore WIP : - Grading API - Webhook - Session

--- src/scripts/utilities/firebase/firestore.ts
+++ src/scripts/utilities/firebase/firestore.ts
@@ -1,14 +1,22 @@
-import { getFirestore, doc, setDoc, getDoc, updateDoc, getDocFromCache, DocumentSnapshot, DocumentData } from "firebase/firestore";
+import { getFirestore, doc, setDoc, getDoc, updateDoc, getDocFromCache, getDocsFromCache, deleteDoc, collection, getDocs, DocumentSnapshot, DocumentData, QuerySnapshot } from "firebase/firestore";
 import { displayInitUser } from "../../ui/tcv-init-user";
 import { tcv_FirebaseAuth } from "./auth";
 import { tcv_HandlerError } from "../display/handler";
 
 export const tcv_FirebaseFirestore = {
-    getData: (parentCollection: string, childDocument: string): Promise<DocumentSnapshot<DocumentData>> => {
+    getData: (parentCollection: string, childDocument?: string): Promise<DocumentSnapshot<DocumentData>> | Promise<QuerySnapshot<DocumentData>> => {
         if (navigator.onLine) {
-            return getDoc(doc(getFirestore(), parentCollection, childDocument));
+            if (childDocument !== "" && childDocument !== undefined && childDocument !== null) {
+                return getDoc(doc(getFirestore(), parentCollection, childDocument));
+            } else {
+                return getDocs(collection(getFirestore(), parentCollection));
+            }
         } else {
-            return getDocFromCache(doc(getFirestore(), parentCollection, childDocument));
+            if (childDocument !== "" && childDocument !== undefined && childDocument !== null) {
+                return getDocFromCache(doc(getFirestore(), parentCollection, childDocument));
+            } else {
+                return getDocsFromCache(collection(getFirestore(), parentCollection));
+            }
         }
     },
     postData: (parentCollection: string, childDocument: string, data): Promise<void> => {
@@ -16,6 +24,9 @@ export const tcv_FirebaseFirestore = {
     },
     updateData: (parentCollection: string, childDocument: string, data): Promise<void> => {
         return updateDoc(doc(getFirestore(), parentCollection, childDocument), data);
+    },
+    deleteData: (parentCollection: string, childDocument: string): Promise<void> => {
+        return deleteDoc(doc(getFirestore(), parentCollection, childDocument));
     },
     initUserData: async (): Promise<boolean> => {
         const user = tcv_FirebaseAuth.currentUser();
