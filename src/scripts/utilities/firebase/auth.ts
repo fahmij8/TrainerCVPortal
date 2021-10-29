b0b4c346baac7520af73b8b7d95ef388a59be13c
fahmij8 <fahmijabbar@upi.edu>
2021-10-29 16:15:06 +0700

    Add offline callback on auth

--- src/scripts/utilities/firebase/auth.ts
+++ src/scripts/utilities/firebase/auth.ts
@@ -1,4 +1,5 @@
 import { getAuth, User, onAuthStateChanged, Auth, GoogleAuthProvider, AuthProvider, signInWithRedirect, signOut } from "firebase/auth";
+import { tcv_HandlerError } from "../display/handler";
 import { tcv_Util } from "../display/util";
 
 export const tcv_FirebaseAuth = {
@@ -32,21 +33,29 @@ export const tcv_FirebaseAuth = {
         });
     },
     login: (): void => {
-        const auth: Auth = getAuth();
-        const provider: AuthProvider = new GoogleAuthProvider();
-        signInWithRedirect(auth, provider);
+        if (navigator.onLine) {
+            const auth: Auth = getAuth();
+            const provider: AuthProvider = new GoogleAuthProvider();
+            signInWithRedirect(auth, provider);
+        } else {
+            tcv_HandlerError.show_NoConfirm("Cannot log-in while you're offline. Please reconnect");
+        }
     },
     logout: async (): Promise<void> => {
-        const auth: Auth = getAuth();
-        signOut(auth).then(() => {
-            if (window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost") {
-                window.history.pushState(null, null, `/`);
-                window.history.pushState({ state: true }, "", `#login`);
-            } else {
-                window.history.pushState(null, null, `/`);
-                window.history.pushState({ state: true }, "", `/login`);
-            }
-            tcv_Util.goToPage("hard");
-        });
+        if (navigator.onLine) {
+            const auth: Auth = getAuth();
+            signOut(auth).then(() => {
+                if (window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost") {
+                    window.history.pushState(null, null, `/`);
+                    window.history.pushState({ state: true }, "", `#login`);
+                } else {
+                    window.history.pushState(null, null, `/`);
+                    window.history.pushState({ state: true }, "", `/login`);
+                }
+                tcv_Util.goToPage("hard");
+            });
+        } else {
+            tcv_HandlerError.show_NoConfirm("Cannot log-out while you're offline. Please reconnect");
+        }
     },
 };
